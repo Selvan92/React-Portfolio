fc63608822316905a8ff3fdf21db9580b0659cb2
Satya Selvan <109838483+Selvan92@users.noreply.github.com>
2024-02-06 21:11:40 +0530

    Update index.js

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -14,11 +14,10 @@ function About() {
             <img src={profileImage} style={{ width: "225px", height: "225px", borderRadius: '50%'}} alt="profile" />
           </div>
           <p>
-            Greetings! I'm Lindsey Martin, a 32-year-old residing in the vibrant city of Los Angeles. I hold a distinguished B.A. in Psychology with a specialization in Neuroscience from the esteemed University of San Francisco, where I completed my studies in 2016. Although I originally migrated to San Francisco for my college education, I found the city so captivating that I decided to call it my home even after graduation.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Condimentum id venenatis a condimentum vitae. Pellentesque diam volutpat commodo sed egestas egestas fringilla. Orci sagittis eu volutpat odio facilisis mauris.Pellentesque habitant morbi tristique senectus et netus et. Cum sociis natoque penatibus et magnis dis parturient montes nascetur. Mauris augue neque gravida in fermentum et sollicitudin ac. Tempor orci d.
           </p>
-          <p>However, with the advent of the COVID pandemic, I found myself oscillating between the bustling streets of Los Angeles and the familiar avenues of San Francisco. This transition offered me the wonderful opportunity to reconnect with my family, who resides in Southern California, and to relish their company during these uncertain times.</p>
-          <p>Beyond my professional commitments, I am an ardent advocate of an active lifestyle. I take immense pleasure in embarking on leisurely strolls accompanied by my beloved canines. Additionally, I relish partaking in brief hiking expeditions with my cherished friends. Engaging in physical exercise brings me great joy, and I diligently dedicate time every day to nurture my well-being. My exercise regimen encompasses a diverse array of activities, including pilates, weight training, yoga, and invigorating high-intensity interval training (HIIT) sessions.</p>
-          <p>By embracing an active and balanced lifestyle, I strive to make the most of my time outside of work, enjoying the wonders of both nature and the thriving communities that surround me.</p>
+          <p> Mi tempus imperdiet nulla malesuada pellentesque elit eget gravida. Nullam vehicula ipsum a arcu cursus. Integer quis auctor elit sed vulputate mi sit. Gravida arcu ac tortor dignissim convallis aenean et tortor. Amet mattis vulputate enim nulla aliquet porttitor. Diam sit amet nisl suscipit adipiscing bibendum. Habitant morbi tristique senectus et netus. Amet facilisis magna etiam tempor orci eu lobortis. Porta nibh venenatis cras sed felis. Consequat interdum varius sit amet mattis vulputate enim nulla. Aliquam sem et tortor consequat. In massa tempor nec feugiat nisl pretium..</p>
+          
         </div>
       </div>
     </section>
